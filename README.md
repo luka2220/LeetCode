bf35e496c50501ec2ffb9d271ec8f0b7880aacbf
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2025-01-04 20:45:52 -0500

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+### Question Log
+- **inc**: incomplete solution
+- **opt**: optimize the solution
+- **✅**: complete
+- **red**: redo question
+
+| Question    | Status      | Topics   | Link |
+| ----------- | ----------- | -------- | ---- |
+| 1570        | red         | Array, Hash Table, Design | [Question](https://leetcode.com/problems/dot-product-of-two-sparse-vectors/description/)
---
+++ java/141/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> store = new HashSet<>();
+        var curr = head;
+
+
+        return false;
+    }
+}
